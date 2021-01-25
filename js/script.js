dced27279b155f96e9e31ac44ccfdac3775b4035
Sujan Chhetri <44917558+sujanchhetri@users.noreply.github.com>
2021-01-25 20:28:43 +0530

    file size support incresed to 100mb

--- js/script.js
+++ js/script.js
@@ -43,8 +43,8 @@ $(function(){
 
 		file = e.target.files[0];
 
-		if(file.size > 1024*1024*10){
-			alert('Please choose files smaller than 10mb, otherwise you may crash your browser.');
+		if(file.size > 1024*100){
+			alert('Please choose files smaller than 100mb, otherwise you may crash your browser.');
 			return;
 		}
 		step(3);
